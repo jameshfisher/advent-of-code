0faa7b03cddc7977e3372263301de182e02991c4
Jim Fisher <jameshfisher@gmail.com>
2017-12-06 00:19:34 +0000

    p1

---
+++ 2017/4/part1.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# --- Day 4: High-Entropy Passphrases ---
+#
+# A new system policy has been put in place
+# that requires all accounts to use a passphrase instead of simply a password.
+# A passphrase consists of a series of words (lowercase letters) separated by spaces.
+#
+# To ensure security, a valid passphrase must contain no duplicate words.
+#
+# For example:
+#
+# aa bb cc dd ee is valid.
+# aa bb cc dd aa is not valid - the word aa appears more than once.
+# aa bb cc dd aaa is valid - aa and aaa count as different words.
+#
+# The system's full passphrase list is available as your puzzle input. How many passphrases are valid?
+
+declare -a passphrase
+declare -i i j passphrase_length total
+while read -a passphrase; do
+  passphrase_length="${#passphrase[@]}"
+  for ((i = 0; i < passphrase_length; i++)); do
+    for ((j = i+1; j < passphrase_length; j++)); do
+      if [ "${passphrase[i]}" = "${passphrase[j]}" ]; then
+        continue 3
+      fi
+    done
+  done
+  ((total++))
+done < input.txt
+printf 'Valid passphrases: %d\n' $total
